d95a937a27a7dbc940476f8f137327fff0ad7590
brianjerry-o1 <bwanga@byupathway.edu>
2026-05-10 15:13:56 +0300

    I HAVE MADE CHAGES FOR WEEK 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // A random number between 1 and 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int guess = 80;
+            int guessCount = 0;
+
+            Console.WriteLine("Welcome to the Guess My Number Game!");
+
+            // Keep looping until correct value found           while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // number of guesses 
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            // replay
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,75 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        // Create a list to store numbers
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+
+        // Keep asking until the user enters 0
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            // Do not add 0 to the list
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Variables for calculations
+        int sum = 0;
+        int largest = numbers[0];
+
+        // Stretch challenge variable
+        int smallestPositive = int.MaxValue;
+
+        // Loop through the list
+        foreach (int number in numbers)
+        {
+            // Calculate sum
+            sum += number;
+
+            // Find largest number
+            if (number > largest)
+            {
+                largest = number;
+            }
+
+            // Find smallest positive number
+            if (number > 0 && number < smallestPositive)
+            {
+                smallestPositive = number;
+            }
+        }
+
+        // Calculate average
+        double average = (double)sum / numbers.Count;
+
+        // Display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
+
+        // Stretch challenge output
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        // Sort the list
+        numbers.Sort();
+
+        Console.WriteLine("The sorted list is:");
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
