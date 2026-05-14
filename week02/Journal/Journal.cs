4d56855b0fd9a9b3c1a640fa20a2d4deb06b63a5
brianjerry-o1 <bwanga@byupathway.edu>
2026-05-14 12:48:08 +0300

    Addition of initial class design stubs for Journal project

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    // Attributes
+    public List<Entry> _entries = new List<Entry>();
+
+    // Behaviors
+    public void AddEntry(Entry newEntry)
+    {
+        // Empty stub
+    }
+
+    public void DisplayAll()
+    {
+        // Empty stub
+    }
+
+    public void SaveToFile(string file)
+    {
+        // Empty stub
+    }
+
+    public void LoadFromFile(string file)
+    {
+        // Empty stub
+    }
+}
